2b9230e38de97bd412798b98bb4fb32b81f909cb
julia1071 <80682741+julia1071@users.noreply.github.com>
2024-07-17 11:58:11 +0200

    case study RDS3

--- run_scenarios_RDS3.py
+++ run_scenarios_RDS3.py
@@ -1,12 +1,15 @@
 import json
 from pathlib import Path
+
+import pandas as pd
+
 import adopt_net0.data_preprocessing as dp
 from adopt_net0.modelhub import ModelHub
 from adopt_net0.result_management.read_results import add_values_to_summary
 
 
 #Run Chemelot cluster case
-execute = 1
+execute = 0
 
 if execute == 1:
     # Specify the path to your input data
@@ -14,37 +17,52 @@ if execute == 1:
     resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Chemelot_cluster"
     json_filepath = Path(casepath) / "ConfigModel.json"
 
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['emissions_minC']
 
-    with open(json_filepath) as json_file:
-        model_config = json.load(json_file)
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
 
-    model_config['optimization']['typicaldays']['N']['value'] = 20
-    model_config['optimization']['objective']['value'] = 'pareto'
-    model_config['optimization']['emission_limit']['value'] = 0
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
 
-    #change save options
-    model_config['reporting']['save_summary_path']['value'] = resultpath
-    model_config['reporting']['save_path']['value'] = resultpath
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
 
 
-    # Write the updated JSON data back to the file
-    with open(json_filepath, 'w') as json_file:
-        json.dump(model_config, json_file, indent=4)
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
 
-    # Construct and solve the model
-    pyhub = ModelHub()
-    pyhub.read_data(casepath)
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
 
-    pyhub.data.model_config['solveroptions']['mipfocus']['value'] = 1
+        if obj == 'emissions_minC':
+            pyhub.data.model_config['solveroptions']['mipfocus']['value'] = 1
 
-    #add casename based on resolution
-    if pyhub.data.model_config['optimization']['typicaldays']['N']['value'] == 0:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'fullres'
-    else:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'TD' + str(pyhub.data.model_config['optimization']['typicaldays']['N']['value'])
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
 
-    #solving
-    pyhub.quick_solve()
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
 
 
 #Run Chemelot ammonia case
@@ -56,37 +74,51 @@ if execute == 1:
     resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Chemelot_ammonia"
     json_filepath = Path(casepath) / "ConfigModel.json"
 
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['emissions_minC']
 
-    with open(json_filepath) as json_file:
-        model_config = json.load(json_file)
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
 
-    model_config['optimization']['typicaldays']['N']['value'] = 20
-    model_config['optimization']['objective']['value'] = 'pareto'
-    model_config['optimization']['emission_limit']['value'] = 0
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
 
-    #change save options
-    model_config['reporting']['save_summary_path']['value'] = resultpath
-    model_config['reporting']['save_path']['value'] = resultpath
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
 
 
-    # Write the updated JSON data back to the file
-    with open(json_filepath, 'w') as json_file:
-        json.dump(model_config, json_file, indent=4)
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
 
-    # Construct and solve the model
-    pyhub = ModelHub()
-    pyhub.read_data(casepath)
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
 
-    pyhub.data.model_config['solveroptions']['mipfocus']['value'] = 1
+        if obj == 'emissions_minC':
 
-    #add casename based on resolution
-    if pyhub.data.model_config['optimization']['typicaldays']['N']['value'] == 0:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'fullres'
-    else:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'TD' + str(pyhub.data.model_config['optimization']['typicaldays']['N']['value'])
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
 
-    #solving
-    pyhub.quick_solve()
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
 
 
 #Run Chemelot ethylene
@@ -98,41 +130,56 @@ if execute == 1:
     resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Chemelot_ethylene"
     json_filepath = Path(casepath) / "ConfigModel.json"
 
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['emissions_minC']
 
-    with open(json_filepath) as json_file:
-        model_config = json.load(json_file)
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
 
-    model_config['optimization']['typicaldays']['N']['value'] = 20
-    model_config['optimization']['objective']['value'] = 'pareto'
-    model_config['optimization']['emission_limit']['value'] = 0
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
 
-    #change save options
-    model_config['reporting']['save_summary_path']['value'] = resultpath
-    model_config['reporting']['save_path']['value'] = resultpath
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
 
 
-    # Write the updated JSON data back to the file
-    with open(json_filepath, 'w') as json_file:
-        json.dump(model_config, json_file, indent=4)
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
 
-    # Construct and solve the model
-    pyhub = ModelHub()
-    pyhub.read_data(casepath)
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
 
-    pyhub.data.model_config['solveroptions']['mipfocus']['value'] = 1
+        if obj == 'emissions_minC':
 
-    #add casename based on resolution
-    if pyhub.data.model_config['optimization']['typicaldays']['N']['value'] == 0:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'fullres'
-    else:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'TD' + str(pyhub.data.model_config['optimization']['typicaldays']['N']['value'])
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
 
-    #solving
-    pyhub.quick_solve()
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
+
 
 
 #Run Zeeland cluster case
-execute = 1
+execute = 0
 
 if execute == 1:
     # Specify the path to your input data
@@ -140,37 +187,160 @@ if execute == 1:
     resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Zeeland_cluster"
     json_filepath = Path(casepath) / "ConfigModel.json"
 
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['emissions_minC']
 
-    with open(json_filepath) as json_file:
-        model_config = json.load(json_file)
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
 
-    model_config['optimization']['typicaldays']['N']['value'] = 20
-    model_config['optimization']['objective']['value'] = 'pareto'
-    model_config['optimization']['emission_limit']['value'] = 0
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
 
-    #change save options
-    model_config['reporting']['save_summary_path']['value'] = resultpath
-    model_config['reporting']['save_path']['value'] = resultpath
-
-
-    # Write the updated JSON data back to the file
-    with open(json_filepath, 'w') as json_file:
-        json.dump(model_config, json_file, indent=4)
-
-    # Construct and solve the model
-    pyhub = ModelHub()
-    pyhub.read_data(casepath)
-
-    pyhub.data.model_config['solveroptions']['mipfocus']['value'] = 1
-
-    #add casename based on resolution
-    if pyhub.data.model_config['optimization']['typicaldays']['N']['value'] == 0:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'fullres'
-    else:
-        pyhub.data.model_config['reporting']['case_name']['value'] = 'TD' + str(pyhub.data.model_config['optimization']['typicaldays']['N']['value'])
-
-    #solving
-    pyhub.quick_solve()
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
 
 
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
 
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
+
+        if obj == 'emissions_minC':
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
+
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Zeeland', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
+
+
+
+#Run Zeeland ammonia case
+execute = 1
+
+if execute == 1:
+    # Specify the path to your input data
+    casepath = "Z:/PyHub/PyHub_casestudies/CM/Zeeland_ammonia"
+    resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Zeeland_ammonia"
+    json_filepath = Path(casepath) / "ConfigModel.json"
+
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['emissions_minC']
+
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
+
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
+
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
+
+
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
+
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
+
+        if obj == 'emissions_minC':
+
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
+
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Zeeland', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Zeeland', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
+
+
+#Run Zeeland ethylene
+execute = 0
+
+if execute == 1:
+    # Specify the path to your input data
+    casepath = "Z:/PyHub/PyHub_casestudies/CM/Zeeland_ethylene"
+    resultpath = "Z:/PyHub/PyHub_results/CM/Cluster_integration/Zeeland_ethylene"
+    json_filepath = Path(casepath) / "ConfigModel.json"
+
+    # objectives = ['costs', 'emissions_minC']
+    objectives = ['costs']
+
+    for obj in objectives:
+        with open(json_filepath) as json_file:
+            model_config = json.load(json_file)
+
+        model_config['optimization']['typicaldays']['N']['value'] = 20
+        model_config['optimization']['objective']['value'] = obj
+        model_config['optimization']['emission_limit']['value'] = 0
+
+        #change save options
+        model_config['reporting']['save_summary_path']['value'] = resultpath
+        model_config['reporting']['save_path']['value'] = resultpath
+
+
+        # Write the updated JSON data back to the file
+        with open(json_filepath, 'w') as json_file:
+            json.dump(model_config, json_file, indent=4)
+
+        # Construct and solve the model
+        pyhub = ModelHub()
+        pyhub.read_data(casepath)
+
+        if obj == 'emissions_minC':
+
+            # add casename
+            pyhub.data.model_config['reporting']['case_name']['value'] = 'minE_refCO2tax'
+
+            # solving
+            pyhub.quick_solve()
+
+        elif obj == 'costs':
+            co2tax = ['ref', 'high']
+
+            for tax in co2tax:
+                # add casename
+                pyhub.data.model_config['reporting']['case_name']['value'] = 'minC_' + tax + 'CO2tax'
+
+                if tax == 'high':
+                    pyhub.data.time_series['clustered']['period1', 'Zeeland', 'CarbonCost', 'global', 'price'] = 250
+                    pyhub.data.time_series['full']['period1', 'Zeeland', 'CarbonCost', 'global', 'price'] = 250
+
+                # solving
+                pyhub.quick_solve()
