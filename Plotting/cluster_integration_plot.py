ac2315e85cbea6c917b03e5f5955ff6fb9bff1f8
julia1071 <juliatiggeloven@gmail.com>
2024-07-19 14:19:09 +0200

    update plot integration and case studies

--- Plotting/cluster_integration_plot.py
+++ Plotting/cluster_integration_plot.py
@@ -5,12 +5,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
+from matplotlib.colors import to_rgba
 from matplotlib.ticker import PercentFormatter
 from adopt_net0 import extract_datasets_from_h5group
 
 # Define the data path
 resultfolder = "Z:/PyHub/PyHub_results/CM/Cluster_integration"
 data_to_excel_path = 'C:/EHubversions/AdOpT-NET0_Julia/Plotting/result_data.xlsx'
+
+# select the type of plot from ['costs_spec', 'costs_spec_cor', 'emissions_spec', 'emissions_spec_cor', 'size']
+plot_type = 'size'
 
 get_data = 0
 
@@ -33,10 +37,8 @@ if get_data == 1:
     # Create the DataFrame with NaN values
     result_data = pd.DataFrame(np.nan, index=index, columns=columns)
 
-    lev0 = ['Chemelot', 'Zeeland']
-
     # Fill the path column using a loop
-    for location in lev0:
+    for location in result_data.columns.levels[0]:
         for typ in result_data.columns.levels[1]:
             if typ != 'standalone':
                 folder_name = f"{location}_{typ}"
@@ -54,11 +56,11 @@ if get_data == 1:
                             if h5_path.exists():
                                 with h5py.File(h5_path, "r") as hdf_file:
                                     df = extract_datasets_from_h5group(hdf_file["design/nodes"])
-                                    tecs = ['NaphthaCracker', 'NaphthaCracker_Electric', 'NaphthaCracker_CC', 'KBRreformer', 'KBRreformer_CC', 'eSMR', 'AEC']
+                                    tecs = ['NaphthaCracker', 'NaphthaCracker_CC', 'NaphthaCracker_Electric', 'KBRreformer', 'KBRreformer_CC', 'eSMR', 'AEC']
                                     for tec in tecs:
                                         output_name = 'size_' + tec
-                                        if ('period1', 'Chemelot', tec, 'size') in df.columns:
-                                            result_data.loc[output_name, (location, typ, scenario)] = df[('period1', 'Chemelot', tec, 'size')].iloc[0]
+                                        if ('period1', location, tec, 'size') in df.columns:
+                                            result_data.loc[output_name, (location, typ, scenario)] = df[('period1', location, tec, 'size')].iloc[0]
                                         else:
                                             result_data.loc[output_name, (location, typ, scenario)] = 0
 
@@ -88,9 +90,18 @@ if get_data == 1:
         products[location]['cracker_tot'] = products[location]['ethylene'] / ethylene_yield
         products[location]['total_product_cor'] = products[location]['ammonia'] + products[location]['cracker_tot']
 
-
+    multipliers = {
+        'NaphthaCracker': 0.3025,
+        'KBR': 0.127848,
+        'eSMR': 0.155736,
+        'AEC': 0.105672
+    }
 
     # Make calculations with results
+    for key, multiplier in multipliers.items():
+        mask = result_data.index.to_series().str.contains(key)
+        result_data.loc[mask] *= multiplier
+
     for location in result_data.columns.levels[0]:
         for scenario in result_data.columns.levels[2]:
             for row in result_data.index:
@@ -99,6 +110,12 @@ if get_data == 1:
                     location, 'ethylene', scenario)] + result_data.loc[row, (location, 'ammonia', scenario)]
                 else:
                     result_data.loc[row, (location, 'standalone', scenario)] = None
+
+                for type in result_data.columns.levels[1]:
+                    for key, multiplier in multipliers.items():
+                        if key in row:
+                            result_data.loc[row, (location, type, scenario)] *= multiplier
+
 
             # get specific costs and emissions
             result_data.loc['costs_spec', (location, 'cluster', scenario)] = result_data.loc['costs_tot', (
@@ -135,6 +152,7 @@ if get_data == 1:
 
     result_data.to_excel(data_to_excel_path)
 
+
 if get_data == 0:
     result_data = pd.read_excel(data_to_excel_path, index_col=0, header=[0, 1, 2])
 
@@ -150,71 +168,199 @@ plt.rcParams.update({
     "ytick.labelsize": 10,
 })
 
-
-#Select data to plot (ethylene will be standalone later)
+# Select data to plot
 locations = ['Chemelot', 'Zeeland']
 types = ['cluster', 'standalone']
 scenarios = ['minC_ref', 'minC_high']
-metric = 'costs_spec'
 
-# Extract the relevant data from the DataFrame and prepare data
-plot_data = result_data.loc[metric, locations[0]]
-plot_data = plot_data.loc[types, scenarios]
+if plot_type in ['costs_spec', 'costs_spec_cor', 'emissions_spec', 'emissions_spec_cor']:
+    metric = plot_type
 
-# Plotting
-fig, ax = plt.subplots(figsize=(10, 6))
+    # Extract the relevant data from the DataFrame and prepare data
+    plot_data = result_data.loc[metric, locations[0]]
+    plot_data = plot_data.loc[types, scenarios]
 
-# Define custom colors and layout
-colors = ['#F1DAC4', '#474973']
-bar_width = 0.15
-n_types = len(types)
-n_scenarios = len(scenarios)
-n_locations = len(locations)
-total_bars = n_types * n_scenarios
+    # Plotting
+    fig, ax = plt.subplots(figsize=(10, 6))
 
-# Set positions of the bars
-index = np.arange(n_locations) * (total_bars + 1) * bar_width
+    # Define custom colors and layout
+    colors = ['#F1DAC4', '#474973']
+    bar_width = 0.15
+    n_types = len(types)
+    n_scenarios = len(scenarios)
+    n_locations = len(locations)
+    total_bars = n_types * n_scenarios
 
-# Create custom patches for the legend
-legend_elements = [
-    plt.Line2D([0], [0], color=colors[0], lw=4, alpha=0.5, label='cluster (reference CO$_2$ tax)'),
-    plt.Line2D([0], [0], color=colors[0], lw=4, label='cluster (high CO$_2$ tax)'),
-    plt.Line2D([0], [0], color=colors[1], lw=4, alpha=0.5, label='standalone (reference CO$_2$ tax)'),
-    plt.Line2D([0], [0], color=colors[1], lw=4,  label='standalone (high CO$_2$ tax)')
-]
+    # Set positions of the bars
+    index = np.arange(n_locations) * (total_bars + 1) * bar_width
 
-# Plot each location
-for loc_idx, location in enumerate(locations):
-    plot_data = result_data.loc[metric, location].loc[types, scenarios]
+    # Create custom patches for the legend
+    legend_elements = [
+        plt.Line2D([0], [0], color=colors[0], lw=4, alpha=0.5, label='cluster (reference CO$_2$ tax)'),
+        plt.Line2D([0], [0], color=colors[0], lw=4, label='cluster (high CO$_2$ tax)'),
+        plt.Line2D([0], [0], color=colors[1], lw=4, alpha=0.5, label='standalone (reference CO$_2$ tax)'),
+        plt.Line2D([0], [0], color=colors[1], lw=4,  label='standalone (high CO$_2$ tax)')
+    ]
 
-    # Plot each scenario for each type
-    for j, scenario in enumerate(scenarios):
-        for i, typ in enumerate(types):
-            position = index[loc_idx] + (i * n_scenarios + j) * bar_width
-            if scenario == 'minC_ref':
-                plt.bar(position, plot_data.loc[typ, scenario], bar_width,
-                        label=None, color=colors[i], alpha=0.5)
-            elif scenario == 'minC_high':
-                plt.bar(position, plot_data.loc[typ, scenario], bar_width,
-                        label=None, color=colors[i])
+    # Plot each location
+    for loc_idx, location in enumerate(locations):
+        plot_data = result_data.loc[metric, location].loc[types, scenarios]
 
-# Adding labels and title
-plt.ylabel('Specific costs [€/tonne product]')
-plt.xticks(index + bar_width * (total_bars - 1) / 2, locations)
-plt.legend(handles=legend_elements, loc='upper right')
+        # Plot each scenario for each type
+        for j, scenario in enumerate(scenarios):
+            for i, typ in enumerate(types):
+                position = index[loc_idx] + (i * n_scenarios + j) * bar_width
+                if scenario == 'minC_ref':
+                    plt.bar(position, plot_data.loc[typ, scenario], bar_width,
+                            label=None, color=colors[i], alpha=0.5)
+                elif scenario == 'minC_high':
+                    plt.bar(position, plot_data.loc[typ, scenario], bar_width,
+                            label=None, color=colors[i])
 
-# Adjust layout
-plt.tight_layout()
+    if metric in ['emissions_spec', 'emissions_spec_cor']:
+        line_styles = ['--', ':']  # Different line styles for different locations
+        for loc_idx, location in enumerate(locations):
+            for i, typ in enumerate(types):
+                minE_value = result_data.loc[metric, location].loc[typ, 'minE']
+                plt.axhline(y=minE_value, color=colors[i], linestyle=line_styles[loc_idx],
+                            label=f'{typ} (minE, {location})')
+
+    # Adding labels and title
+    plt.xticks(index + bar_width * (total_bars - 1) / 2, locations)
+    plt.legend(handles=legend_elements + [
+        plt.Line2D([0], [0], color=colors[0], linestyle='--', label='cluster minimum emissions (Chemelot)'),
+        plt.Line2D([0], [0], color=colors[0], linestyle=':', label='cluster minimum emissions (Zeeland)'),
+        plt.Line2D([0], [0], color=colors[1], linestyle='--', label='standalone minimum emissions (Chemelot)'),
+        plt.Line2D([0], [0], color=colors[1], linestyle=':', label='standalone minimum emissions (Zeeland)')
+    ], loc='upper center', ncol=2)
+
+    # define filename
+    if 'cost' in metric:
+        plt.ylabel('Specific costs [€/tonne product]')
+        filename = 'integration_costs'
+    elif 'emission' in metric:
+        plt.ylabel('Specific emissions [kg CO$_2$/tonne product]')
+        filename = 'integration_emissions'
+    if 'cor' in metric:
+        filename = filename + '_cor'
+
+    # Adjust layout
+    plt.tight_layout()
+
+    #save the file
+    saveas = 'pdf'
+
+    if saveas == 'svg':
+        savepath = f'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/{filename}.svg'
+        plt.savefig(savepath, format='svg')
+    elif saveas == 'pdf':
+        savepath = f'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/{filename}.pdf'
+        plt.savefig(savepath, format='pdf')
 
 
-saveas = '0'
+elif plot_type == 'size':
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5), sharey=True)
 
-if saveas == 'svg':
-    savepath = 'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/' + 'integration_costs.svg'
-    plt.savefig(savepath, format='svg')
-if saveas == 'pdf':
-    savepath = 'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/' + 'integration_costs.pdf'
-    plt.savefig(savepath, format='pdf')
+    tecs = {'ethylene': ['NaphthaCracker', 'NaphthaCracker_Electric', 'NaphthaCracker_CC'],
+            'ammonia': ['KBRreformer', 'KBRreformer_CC', 'eSMR', 'AEC']}
+
+    # Define custom colors for the tecs
+    # colors = ['#3B252C', '#8F6593', '#AEA4BF', '#CDCDCD']
+    # colors = ['#422439', '#BDA0BC', '#C3D2D5', '#C1F7DC']
+    colors = ['#48525B', '#5277B7', '#FFBC47', '#639051']
+
+
+    # Adjust bar width and gap between groups
+    bar_width = 0.5  # Make the bars wider
+    group_gap = 0.5  # Adjust the gap between groups
+    bar_gap = 0.01
+    opacity = 1
+
+    # Preprocess tec labels
+    def format_label(label):
+        label = label.replace('_', ' ')
+        if 'NaphthaCracker' in label:
+            label = label.replace('NaphthaCracker', 'Naphtha Cracker')
+        if 'KBRreformer' in label:
+            label = label.replace('KBRreformer', 'KBR Reformer')
+        if 'CC' in label:
+            label = label.replace('CC', 'with CC')
+        return label
+
+
+    formatted_tecs = {product: [format_label(tec) for tec in tecs[product]] for product in tecs}
+
+    for ax, product in zip(axes, tecs):
+        n_locations = len(locations)
+        n_scenarios = len(scenarios)
+        n_types = len(types)
+
+        # Calculate the total number of bars per location
+        total_bars_per_loc = n_scenarios * n_types
+
+        # Calculate the positions for each group, ensuring no extra space within groups
+        index = np.arange(n_locations) * (total_bars_per_loc * bar_width + group_gap)
+
+        # Loop through each location
+        for loc_idx, location in enumerate(reversed(locations)):
+            for typ_idx, typ in enumerate(reversed(types)):
+                for sc_idx, scenario in enumerate(reversed(scenarios)):
+                    position = index[loc_idx] + (sc_idx + typ_idx * n_scenarios) * (bar_width + bar_gap)
+                    bottom = 0
+
+                    for tec_idx, tec in enumerate(tecs[product]):
+                        metric = f'size_{tec}'
+                        value = result_data.loc[metric, (location, typ, scenario)]
+
+                        if scenario == 'minC_ref':
+                            opacity = 0.5
+                        else:
+                            opacity = 1
+
+                        color = to_rgba(colors[tec_idx], alpha=opacity)
+
+                        # Plot each technology as a stacked horizontal bar
+                        ax.barh(position, value, bar_width,
+                                label=formatted_tecs[product][tec_idx] if loc_idx == 0 and typ_idx == 0 and sc_idx == 0 else "",
+                                color=color, left=bottom)
+                        bottom += value
+
+        # ax.set_title(f'{product.capitalize()} Production')
+        # tick_positions = np.arange(n_locations) * (total_bars_per_loc * bar_width + group_gap) + (
+        #         total_bars_per_loc * bar_width / 2) - 0.23
+        # ax.set_yticks(tick_positions)
+        # ax.set_yticklabels(reversed(locations), rotation=0)
+
+        total_ticks = len(locations) * len(types)
+        tick_positions = np.arange(total_ticks) * (total_bars_per_loc/2 * bar_width) + (total_bars_per_loc/2 * bar_width / 2) - 0.23
+        tick_positions[2] = tick_positions[2] + group_gap
+        tick_positions[3] = tick_positions[3] + group_gap
+        ytick_labels = [f'{loc}-{typ}' for loc in reversed(locations) for typ in reversed(types)]
+        ax.set_yticks(tick_positions)
+        ax.set_yticklabels(ytick_labels, rotation=0)
+
+
+        ax.legend(loc='upper right')
+
+        if product == 'ammonia':  # To avoid duplicate legends
+            ax.set_xlabel('Size [t NH$_3$/h]')
+        elif product == 'ethylene':
+            ax.set_xlabel('Size [t C$_2$H$_4$/h]')
+
+    axes[0].set_xlim(0, 225)
+
+    # Adjust layout
+    plt.tight_layout()
+
+    # save the file
+    saveas = 'pdf'
+
+    if saveas == 'svg':
+        savepath = f'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/installed_capacities.svg'
+        plt.savefig(savepath, format='svg')
+    elif saveas == 'pdf':
+        savepath = f'C:/Users/5637635/Documents/OneDrive - Universiteit Utrecht/Images and graphs/Collection CM/Paper/installed_capacities.pdf'
+        plt.savefig(savepath, format='pdf')
 
 #show plot
 plt.show()
--- create_casestudies.py
+++ create_casestudies.py
@@ -47,35 +47,114 @@ if execute == 1:
 
         # CO2 export
         dp.fill_carrier_data(casepath, value_or_data=115, columns=['Export limit'], carriers=['CO2'])
-        dp.fill_carrier_data(casepath, value_or_data=-61.90, columns=['Export price'], carriers=['CO2'])
+    dp.fill_carrier_data(casepath, value_or_data=-63.78, columns=['Export price'], carriers=['CO2'])
 
-        # Constant prices
-        dp.fill_carrier_data(casepath, value_or_data=100, columns=['Import price'], carriers=['methane'])
+        # # Constant prices
+        # dp.fill_carrier_data(casepath, value_or_data=100, columns=['Import price'], carriers=['methane'])
+        # dp.fill_carrier_data(casepath, value_or_data=732, columns=['Import price'], carriers=['naphtha'])
+        #
+        # # Electricity price from file
+        # el_load_path = Path(datapath) / 'import_data' / 'Electricity_data_CM.csv'
+        # el_importdata = pd.read_csv(el_load_path, sep=';', header=0, nrows=8760)
+        # el_price = el_importdata.iloc[:, 0]
+        # el_emissionrate = el_importdata.iloc[:, 3]
+        #
+        # dp.fill_carrier_data(casepath, value_or_data=el_price, columns=['Import price'], carriers=['electricity'])
+        # dp.fill_carrier_data(casepath, value_or_data=el_emissionrate, columns=['Import emission factor'], carriers=['electricity'])
+        #
+        #
+        # #carbon tax
+        # file_path = Path(casepath) / 'period1' / "node_data" / 'Chemelot' / 'CarbonCost.csv'
+        # data = pd.read_csv(file_path, delimiter=';')
+        #
+        # # Set the price to 150.31 and subsidy to 0 for all rows
+        # data['price'] = 150.31
+        # data['subsidy'] = 0
+        #
+        # # Save the modified CSV file
+        # data.to_csv(file_path, index=False, sep=';')
+
+
+#Create data Chemelot standalone
+execute = 0
+
+if execute == 1:
+    # Specify the path to your input data
+    casepath = "Z:/PyHub/PyHub_casestudies/CM/Chemelot_ethylene"
+    datapath = "Z:/PyHub/PyHub_data/CM/240624_CM"
+
+    carrier = 'ethylene'
+
+    dp.fill_carrier_data(casepath, value_or_data=0)
+
+    if carrier == 'ammonia':
+        # Demand data
+        dp.fill_carrier_data(casepath, value_or_data=135, columns=['Demand'], carriers=['ammonia'])
+        dp.fill_carrier_data(casepath, value_or_data=45, columns=['Demand'], carriers=['CO2'])
+        dp.fill_carrier_data(casepath, value_or_data=5, columns=['Demand'], carriers=['electricity'])
+
+        # No import limit
+        dp.fill_carrier_data(casepath, value_or_data=2000/2, columns=['Import limit'],
+                             carriers=['electricity'])
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Import limit'],
+                             carriers=['methane', "CO2"])
+
+        # No export limit
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Export limit'],
+                             carriers=['nitrogen', 'oxygen', 'heatlowT', 'steam'])
+
+    elif carrier == 'ethylene':
+        # Demand data
+        dp.fill_carrier_data(casepath, value_or_data=150, columns=['Demand'], carriers=['ethylene'])
+        dp.fill_carrier_data(casepath, value_or_data=64.6, columns=['Demand'], carriers=['steam'])
+        dp.fill_carrier_data(casepath, value_or_data=81.4, columns=['Demand'], carriers=['electricity'])
+        dp.fill_carrier_data(casepath, value_or_data=6.9, columns=['Demand'], carriers=['crackergas'])
+
+        # No import limit
+        dp.fill_carrier_data(casepath, value_or_data=2000/2, columns=['Import limit'],
+                             carriers=['electricity'])
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Import limit'],
+                             carriers=['methane', "CO2"])
+
+        # No export limit
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Export limit'], carriers=['heatlowT', 'steam', 'crackergas'])
+        dp.fill_carrier_data(casepath, value_or_data=0.203, columns=['Export emission factor'],
+                             carriers=['crackergas'])
+
+    # CO2 export
+    dp.fill_carrier_data(casepath, value_or_data=115/2, columns=['Export limit'], carriers=['CO2'])
+    dp.fill_carrier_data(casepath, value_or_data=-63.78, columns=['Export price'], carriers=['CO2'])
+
+    # Constant prices
+    dp.fill_carrier_data(casepath, value_or_data=100, columns=['Import price'], carriers=['methane'])
+    if carrier == 'ethylene':
         dp.fill_carrier_data(casepath, value_or_data=732, columns=['Import price'], carriers=['naphtha'])
 
-        # Electricity price from file
-        el_load_path = Path(datapath) / 'import_data' / 'Electricity_data_CM.csv'
-        el_importdata = pd.read_csv(el_load_path, sep=';', header=0, nrows=8760)
-        el_price = el_importdata.iloc[:, 0]
-        el_emissionrate = el_importdata.iloc[:, 3]
+    # Electricity price from file
+    el_load_path = Path(datapath) / 'import_data' / 'Electricity_data_CM.csv'
+    el_importdata = pd.read_csv(el_load_path, sep=';', header=0, nrows=8760)
+    el_price = el_importdata.iloc[:, 0]
+    el_emissionrate = el_importdata.iloc[:, 3]
 
-        dp.fill_carrier_data(casepath, value_or_data=el_price, columns=['Import price'], carriers=['electricity'])
-        dp.fill_carrier_data(casepath, value_or_data=el_emissionrate, columns=['Import emission factor'], carriers=['electricity'])
+    dp.fill_carrier_data(casepath, value_or_data=el_price, columns=['Import price'], carriers=['electricity'])
+    dp.fill_carrier_data(casepath, value_or_data=el_emissionrate, columns=['Import emission factor'], carriers=['electricity'])
 
 
-        #carbon tax
-        file_path = Path(casepath) / 'period1' / "node_data" / 'Chemelot' / 'CarbonCost.csv'
-        data = pd.read_csv(file_path, delimiter=';')
+    #carbon tax
+    file_path = Path(casepath) / 'period1' / "node_data" / 'Chemelot' / 'CarbonCost.csv'
+    data = pd.read_csv(file_path, delimiter=';')
 
-        # Set the price to 150.31 and subsidy to 0 for all rows
-        data['price'] = 150.31
-        data['subsidy'] = 0
+    # Set the price to 150.31 and subsidy to 0 for all rows
+    data['price'] = 150.31
+    data['subsidy'] = 0
 
-        # Save the modified CSV file
-        data.to_csv(file_path, index=False, sep=';')
+    # Save the modified CSV file
+    data.to_csv(file_path, index=False, sep=';')
+
+
 
 #Create data Zeeland cluster
-execute = 1
+execute = 0
 
 if execute == 1:
     # Specify the path to your input data
@@ -115,11 +194,84 @@ if execute == 1:
 
     # CO2 export
     dp.fill_carrier_data(casepath, value_or_data=171, columns=['Export limit'], carriers=['CO2'])
-    dp.fill_carrier_data(casepath, value_or_data=-59.66, columns=['Export price'], carriers=['CO2'])
+    dp.fill_carrier_data(casepath, value_or_data=-54.56, columns=['Export price'], carriers=['CO2'])
+
+        # # Constant prices
+        # dp.fill_carrier_data(casepath, value_or_data=100, columns=['Import price'], carriers=['methane'])
+        # dp.fill_carrier_data(casepath, value_or_data=732, columns=['Import price'], carriers=['naphtha'])
+        #
+        # # Electricity price from file
+        # el_load_path = Path(datapath) / 'import_data' / 'Electricity_data_CM.csv'
+        # el_importdata = pd.read_csv(el_load_path, sep=';', header=0, nrows=8760)
+        # el_price = el_importdata.iloc[:, 1]
+        # el_emissionrate = el_importdata.iloc[:, 3]
+        #
+        # dp.fill_carrier_data(casepath, value_or_data=el_price, columns=['Import price'], carriers=['electricity'])
+        # dp.fill_carrier_data(casepath, value_or_data=el_emissionrate, columns=['Import emission factor'], carriers=['electricity'])
+        #
+        #
+        # #carbon tax
+        # file_path = Path(casepath) / 'period1' / "node_data" / 'Zeeland' / 'CarbonCost.csv'
+        # data = pd.read_csv(file_path, delimiter=';')
+        #
+        # # Set the price to 150.31 and subsidy to 0 for all rows
+        # data['price'] = 150.31
+        # data['subsidy'] = 0
+        #
+        # # Save the modified CSV file
+        # data.to_csv(file_path, index=False, sep=';')
+
+
+#Create data Zeeland standalone
+execute = 1
+
+if execute == 1:
+    # Specify the path to your input data
+    casepath = "Z:/PyHub/PyHub_casestudies/CM/Zeeland_ammonia"
+    datapath = "Z:/PyHub/PyHub_data/CM/240624_CM"
+
+    carrier = 'ammonia'
+
+    dp.fill_carrier_data(casepath, value_or_data=0)
+
+    # Demand data
+    dp.fill_carrier_data(casepath, value_or_data=208, columns=['Demand'], carriers=[carrier])
+
+    if carrier == 'ammonia':
+        dp.fill_carrier_data(casepath, value_or_data=111, columns=['Demand'], carriers=['CO2'])
+        dp.fill_carrier_data(casepath, value_or_data=12.4, columns=['Demand'], carriers=['electricity'])
+
+        # No import limit
+        dp.fill_carrier_data(casepath, value_or_data=3000/2, columns=['Import limit'], carriers=['electricity'])
+        dp.fill_carrier_data(casepath, value_or_data=3000, columns=['Import limit'],
+                             carriers=['methane', "CO2"])
+
+        # No export limit
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Export limit'], carriers=['nitrogen', 'oxygen', 'heatlowT', 'steam', 'crackergas'])
+
+    elif carrier == 'ethylene':
+        dp.fill_carrier_data(casepath, value_or_data=570.2, columns=['Demand'], carriers=['steam'])
+        dp.fill_carrier_data(casepath, value_or_data=80.9, columns=['Demand'], carriers=['electricity'])
+
+        # No import limit
+        dp.fill_carrier_data(casepath, value_or_data=3000/2, columns=['Import limit'], carriers=['electricity'])
+        dp.fill_carrier_data(casepath, value_or_data=3000, columns=['Import limit'],
+                             carriers=['methane', "CO2"])
+
+        # No export limit
+        dp.fill_carrier_data(casepath, value_or_data=2000, columns=['Export limit'],
+                             carriers=['oxygen', 'heatlowT', 'steam', 'crackergas'])
+        dp.fill_carrier_data(casepath, value_or_data=0.203, columns=['Export emission factor'],
+                             carriers=['crackergas'])
+
+    # CO2 export
+    dp.fill_carrier_data(casepath, value_or_data=171/2, columns=['Export limit'], carriers=['CO2'])
+    dp.fill_carrier_data(casepath, value_or_data=-54.56, columns=['Export price'], carriers=['CO2'])
 
     # Constant prices
     dp.fill_carrier_data(casepath, value_or_data=100, columns=['Import price'], carriers=['methane'])
-    dp.fill_carrier_data(casepath, value_or_data=732, columns=['Import price'], carriers=['naphtha'])
+    if carrier == 'ethylene':
+        dp.fill_carrier_data(casepath, value_or_data=732, columns=['Import price'], carriers=['naphtha'])
 
     # Electricity price from file
     el_load_path = Path(datapath) / 'import_data' / 'Electricity_data_CM.csv'
