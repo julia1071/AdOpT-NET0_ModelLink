765495c8e37b8ecbdd7dec6ef1c32e3e84aaa80e
julia1071 <juliatiggeloven@gmail.com>
2024-11-04 11:36:04 +0100

    run separate pathways

--- run_separate-pathways.py
+++ run_separate-pathways.py
@@ -7,7 +7,6 @@ import adopt_net0.data_preprocessing as dp
 from adopt_net0.modelhub import ModelHub
 from adopt_net0.result_management.read_results import add_values_to_summary
 
-
 #Run Chemelot pathways
 execute = 1
 
@@ -21,57 +20,66 @@ if execute == 1:
 
     co2tax = ['ref']
     DD = [0]
-    pathways = {"conventional": ["SteamReformer", "HaberBosch", "NaphthaCracker"],
-                "electric": [],
-                "methanol": [],
-                "hydrocarbon_upgrading": [],
-                "auxiliary": ["Boiler_Industrial_NG", "Boiler_El",]
-                }
+    pathways_ammonia = {"conventional": ["SteamReformer", "HaberBosch"],
+                        "CC": ["SteamReformer_CC", "HaberBosch"],
+                        "electric": ["ElectricSMR_m", "WGS_m",  "HaberBosch", "ASU"],
+                        "electrolyzer": ["AEC", "HaberBosch", "ASU"],
+                        }
+    pathways_ethylene = {"conventional": ["NaphthaCracker"],
+                        "CC": ["NaphthaCracker_CC"],
+                        "electric": ["NaphthaCracker_Electric"],
+                        "methanol1": ["RWGS", "MeOHsynthesis", "MTO"],
+                        "methanol2": ["MPW2methanol", "MTO"],
+                        "hydrocarbon_upgrading": ["EDH", "PDH"],
+                        }
+    pathways_auxiliary = ["Boiler_Industrial_NG", "Boiler_El",
+                          "Storage_Ammonia", "Storage_CO2", "Storage_Ethylene", "Storage_H2",
+                          "Storage_Battery", "Storage_Propylene",
+                          "CO2toEmission", "feedgas_mixer", "naphtha_mixer", "PE_mixer", "CO2_mixer",
+                          "HBfeed_mixer", "syngas_mixer"]
 
-    for tax in co2tax:
-        for nr in DD:
-            # change config file
-            with open(config_filepath) as json_file:
-                model_config = json.load(json_file)
+    for ammonia_key, ammonia_tech in pathways_ammonia.items():
+        for ethylene_key, ethylene_tech in pathways_ethylene.items():
+            # Combine ammonia, ethylene, and auxiliary pathways
+            combined_tech = ammonia_tech + ethylene_tech + pathways_auxiliary
 
-            model_config['optimization']['typicaldays']['N']['value'] = nr
-            model_config['optimization']['objective']['value'] = 'costs'
+            for tax in co2tax:
+                for nr in DD:
+                    # change config file
+                    with open(config_filepath) as json_file:
+                        model_config = json.load(json_file)
 
-            # Write the updated JSON data back to the file
-            with open(config_filepath, 'w') as json_file:
-                json.dump(model_config, json_file, indent=4)
+                    model_config['optimization']['typicaldays']['N']['value'] = nr
+                    model_config['optimization']['objective']['value'] = 'costs'
 
-            # change technologies
-            with open(config_filepath) as json_file:
-                model_config = json.load(json_file)
+                    # Write the updated JSON data back to the file
+                    with open(config_filepath, 'w') as json_file:
+                        json.dump(model_config, json_file, indent=4)
 
-            model_config['optimization']['typicaldays']['N']['value'] = nr
-            model_config['optimization']['objective']['value'] = 'costs'
+                    # change technologies
+                    with open(tech_filepath) as json_file:
+                        techs = json.load(json_file)
 
-            # Write the updated JSON data back to the file
-            with open(config_filepath, 'w') as json_file:
-                json.dump(model_config, json_file, indent=4)
+                    techs["new"] = combined_tech
 
+                    # Write the updated JSON data back to the file
+                    with open(tech_filepath, 'w') as json_file:
+                        json.dump(techs, json_file, indent=4)
 
-            # Construct and solve the model
-            pyhub = ModelHub()
-            pyhub.read_data(casepath)
+                    # Construct and solve the model
+                    pyhub = ModelHub()
+                    pyhub.read_data(casepath)
 
-            if tax == 'high':
-                if nr != 0:
-                    pyhub.data.time_series['clustered']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
-                pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+                    if tax == 'high':
+                        if nr != 0:
+                            pyhub.data.time_series['clustered']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
+                        pyhub.data.time_series['full']['period1', 'Chemelot', 'CarbonCost', 'global', 'price'] = 250
 
-            #add casename based on resolution
-            if pyhub.data.model_config['optimization']['typicaldays']['N']['value'] == 0:
-                pyhub.data.model_config['reporting']['case_name']['value'] = 'fullres'
-            else:
-                pyhub.data.model_config['reporting']['case_name']['value'] = 'DD' + str(pyhub.data.model_config['optimization']['typicaldays']['N']['value'])
+                    #add casename based tech combinition
+                    pyhub.data.model_config['reporting']['case_name']['value'] = 'a_' + ammonia_key + '_e_' + ethylene_key
 
-            #solving
-            pyhub.quick_solve()
-
-
+                    #solving
+                    pyhub.quick_solve()
 
 #Run Chemelot cluster case
 execute = 0
@@ -96,7 +104,6 @@ if execute == 1:
         #change save options
         model_config['reporting']['save_summary_path']['value'] = resultpath
         model_config['reporting']['save_path']['value'] = resultpath
-
 
         # Write the updated JSON data back to the file
         with open(json_filepath, 'w') as json_file:
