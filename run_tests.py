2966dfdbe527fc327cbe1ac223ac72915f973630
julia1071 <80682741+julia1071@users.noreply.github.com>
2025-04-08 14:34:58 +0200

    update time limit

--- run_tests.py
+++ run_tests.py
@@ -48,7 +48,7 @@ if execute == 1:
         model_config['optimization']['scope_three_analysis'] = scope3
 
         # solver settings
-        model_config['solveroptions']['timelim']['value'] = 240
+        model_config['solveroptions']['timelim']['value'] = 24*30
         model_config['solveroptions']['mipgap']['value'] = 0.01
         model_config['solveroptions']['threads']['value'] = 10
 
