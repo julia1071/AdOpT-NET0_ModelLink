96e587b1591b6ca556bfec638b2d836bb4b0734a
julia1071 <juliatiggeloven@gmail.com>
2025-04-22 11:16:54 +0200

    run tight emissions

--- run_TightEmission.py
+++ run_TightEmission.py
@@ -22,12 +22,12 @@ if execute == 1:
     run_with_emission_limit = 1
     # intervals = ['2040', '2050']
     intervals = ['2030', '2040', '2050']
-    interval_emissionLim = {'2030': 0.8, '2040': 0.5, '2050': 0}
+    interval_emissionLim = {'2030': 0.75, '2040': 0.5, '2050': 0}
     nr_DD_days = 10
     prev_from_file = 0
-    emission_2030 = 531218.1986
+    emission_2030 = 522537.2155
     h5_path_prev = Path(
-        "Z:/AdOpt_NET0/AdOpt_results/MY/EmissionLimit Brownfield/OptBIO/20250326093222_2030_minC_DD10-1/optimization_results.h5")
+        "Z:/AdOpt_NET0/AdOpt_results/MY/EmissionLimit Brownfield/TightEmission/20250416112938_2030_minC_DD10-1/optimization_results.h5")
     pyhub = {}
 
     for i, interval in enumerate(intervals):
@@ -38,21 +38,11 @@ if execute == 1:
             model_config = json.load(json_file)
 
         model_config['optimization']['typicaldays']['N']['value'] = nr_DD_days
+        model_config['optimization']['objective']['value'] = "costs_emissionlimit"
 
-        if interval == '2030':
-            model_config['optimization']['objective']['value'] = 'costs'
-        else:
-            prev_interval = intervals[i - 1]
-            model_config['optimization']['objective']['value'] = "costs_emissionlimit"
-            if interval == '2040' and prev_from_file:
-                limit = interval_emissionLim[interval] * emission_2030
-            else:
-                if nr_DD_days > 0:
-                    limit = interval_emissionLim[interval] * pyhub[prev_interval].model[
-                        'clustered'].var_emissions_net.value
-                else:
-                    limit = interval_emissionLim[interval] * pyhub[prev_interval].model['full'].var_emissions_net.value
-            model_config['optimization']['emission_limit']['value'] = limit
+        # Set emission limit based on original 2030 case
+        limit = interval_emissionLim[interval] * emission_2030
+        model_config['optimization']['emission_limit']['value'] = limit
 
         # Scope 3 analysis yes/no
         model_config['optimization']['scope_three_analysis'] = scope3
@@ -80,6 +70,8 @@ if execute == 1:
                 installed_capacities_existing(pyhub, interval, prev_interval, 'Chemelot', casepath_interval)
 
         # Construct and solve the model
+        if interval == '2030':
+            interval = interval + '_tight'
         pyhub[interval] = ModelHub()
         pyhub[interval].read_data(casepath_interval)
 
@@ -98,58 +90,58 @@ if execute == 1:
         pyhub[interval].construct_balances()
         pyhub[interval].solve()
 
-        if interval == '2030':
-            casepath_interval = casepath + interval
-            json_filepath = Path(casepath_interval) / "ConfigModel.json"
-
-            with open(json_filepath) as json_file:
-                model_config = json.load(json_file)
-
-            model_config['optimization']['objective']['value'] = "costs_emissionlimit"
-
-            if nr_DD_days > 0:
-                limit = interval_emissionLim[interval] * pyhub[interval].model[
-                    'clustered'].var_emissions_net.value
-            else:
-                limit = interval_emissionLim[interval] * pyhub[interval].model['full'].var_emissions_net.value
-            model_config['optimization']['emission_limit']['value'] = limit
-
-            # Scope 3 analysis yes/no
-            model_config['optimization']['scope_three_analysis'] = scope3
-
-            # solver settings
-            model_config['solveroptions']['timelim']['value'] = 24 * 30
-            model_config['solveroptions']['mipgap']['value'] = 0.01
-            model_config['solveroptions']['threads']['value'] = 12
-            model_config['solveroptions']['nodefilestart']['value'] = 200
-
-            # change save options
-            model_config['reporting']['save_summary_path']['value'] = resultpath + sensitivity
-            model_config['reporting']['save_path']['value'] = resultpath + sensitivity
-
-            # Write the updated JSON data back to the file
-            with open(json_filepath, 'w') as json_file:
-                json.dump(model_config, json_file, indent=4)
-
-            # Construct and solve the model
-            interval_tight = interval + '_tight'
-            pyhub[interval_tight] = ModelHub()
-            pyhub[interval_tight].read_data(casepath_interval)
-
-            # Set case name
-            if nr_DD_days > 0:
-                pyhub[interval_tight].data.model_config['reporting']['case_name'][
-                    'value'] = (interval_tight + '_minC_' +
-                                'DD' + str(
-                            pyhub[interval_tight].data.model_config['optimization']['typicaldays']['N']['value']))
-
-            else:
-                pyhub[interval].data.model_config['reporting']['case_name'][
-                    'value'] = interval_tight + '_minC_fullres'
-
-            # Start brownfield optimization
-            pyhub[interval_tight].construct_model()
-            pyhub[interval_tight].construct_balances()
-            pyhub[interval_tight].solve()
+        # if interval == '2030':
+        #     casepath_interval = casepath + interval
+        #     json_filepath = Path(casepath_interval) / "ConfigModel.json"
+        #
+        #     with open(json_filepath) as json_file:
+        #         model_config = json.load(json_file)
+        #
+        #     model_config['optimization']['objective']['value'] = "costs_emissionlimit"
+        #
+        #     if nr_DD_days > 0:
+        #         limit = interval_emissionLim[interval] * pyhub[interval].model[
+        #             'clustered'].var_emissions_net.value
+        #     else:
+        #         limit = interval_emissionLim[interval] * pyhub[interval].model['full'].var_emissions_net.value
+        #     model_config['optimization']['emission_limit']['value'] = limit
+        #
+        #     # Scope 3 analysis yes/no
+        #     model_config['optimization']['scope_three_analysis'] = scope3
+        #
+        #     # solver settings
+        #     model_config['solveroptions']['timelim']['value'] = 24 * 30
+        #     model_config['solveroptions']['mipgap']['value'] = 0.01
+        #     model_config['solveroptions']['threads']['value'] = 12
+        #     model_config['solveroptions']['nodefilestart']['value'] = 200
+        #
+        #     # change save options
+        #     model_config['reporting']['save_summary_path']['value'] = resultpath + sensitivity
+        #     model_config['reporting']['save_path']['value'] = resultpath + sensitivity
+        #
+        #     # Write the updated JSON data back to the file
+        #     with open(json_filepath, 'w') as json_file:
+        #         json.dump(model_config, json_file, indent=4)
+        #
+        #     # Construct and solve the model
+        #     interval_tight = interval + '_tight'
+        #     pyhub[interval_tight] = ModelHub()
+        #     pyhub[interval_tight].read_data(casepath_interval)
+        #
+        #     # Set case name
+        #     if nr_DD_days > 0:
+        #         pyhub[interval_tight].data.model_config['reporting']['case_name'][
+        #             'value'] = (interval_tight + '_minC_' +
+        #                         'DD' + str(
+        #                     pyhub[interval_tight].data.model_config['optimization']['typicaldays']['N']['value']))
+        #
+        #     else:
+        #         pyhub[interval].data.model_config['reporting']['case_name'][
+        #             'value'] = interval_tight + '_minC_fullres'
+        #
+        #     # Start brownfield optimization
+        #     pyhub[interval_tight].construct_model()
+        #     pyhub[interval_tight].construct_balances()
+        #     pyhub[interval_tight].solve()
 
 
